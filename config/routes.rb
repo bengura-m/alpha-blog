80e8b7ec5025077af96e784d64a7525608e1eeac
bengura-m <bengura.mitchell@gmail.com>
2022-05-17 15:39:24 +0800

    Performed CRUD action on the blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]
-  get 'articles', to: 'articles#show'
-
-  root 'pages#home'
+  root 'pages#about'
   get 'about', to: 'pages#about'
+  resources :articles #only: [:show, :index, :new, :create, :edit, :update , :destroy]
+  get 'articles', to: 'articles#show'
+  post 'articles', to: 'articles#new'
+  post 'articles', to: 'articles#edit'
+  post 'articles', to: 'articles#eupdate'
 end
